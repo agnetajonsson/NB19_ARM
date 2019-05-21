1b0c95ebc019d8b9e8529576d8e7896566597428
Agneta Jonsson <agneta.jonsson1@hotmail.com>
2019-05-21 13:14:56 +0200

    Lagt till WL

--- NB19_ARM/Program.cs
+++ NB19_ARM/Program.cs
@@ -7,6 +7,7 @@ namespace NB19_ARM
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(HejHej från Agneta);
         }
     }
 }
